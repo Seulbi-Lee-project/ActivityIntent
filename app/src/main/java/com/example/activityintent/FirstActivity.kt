c4ec0b00b3095ecb4ba86f37a5e9d374d7f332cf
Seulbi-Lee-project <ifssfws@gmail.com>
2023-08-09 10:55:11 +0900

    연습2

--- app/src/main/java/com/example/activityintent/FirstActivity.kt
+++ app/src/main/java/com/example/activityintent/FirstActivity.kt
@@ -1,11 +1,20 @@
 package com.example.activityintent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        var btn = findViewById<Button>(R.id.buttonFirstActivity)
+        btn.setOnClickListener {
+            var intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/activityintent/SecondActivity.kt
+++ app/src/main/java/com/example/activityintent/SecondActivity.kt
@@ -2,10 +2,16 @@ package com.example.activityintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        var btn = findViewById<Button>(R.id.buttonSecondActivity)
+        btn.setOnClickListener {
+            finish()
+        }
     }
 }
