8aba5a84021287f5aa9472f9867424a526c71d0a
Seulbi-Lee-project <ifssfws@gmail.com>
2023-08-09 11:03:53 +0900

    연습3

--- app/src/main/java/com/example/activityintent/FirstActivity.kt
+++ app/src/main/java/com/example/activityintent/FirstActivity.kt
@@ -1,8 +1,10 @@
 package com.example.activityintent
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class FirstActivity : AppCompatActivity() {
@@ -15,6 +17,23 @@ class FirstActivity : AppCompatActivity() {
             var intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
-
+    }
+    /**
+     * 버튼 클릭 이벤트 처리
+     * @param view : 클릭된 버튼 객체
+     *
+     * 클릭된 버튼 객체가 무엇인지를 id를 통해 인지하여, 두 가지 다른 인텐트 객체를 생성
+     */
+    fun doOnBtnClick(view: View) {
+        when (view.getId()) {
+            R.id.buttonDialActivity -> {                 // 114 전화번호로 다이얼 작업을 수행할 수 있도록 인텐트 설정
+                val call_intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:114"))
+                startActivity(call_intent)
+            }
+            R.id.buttonMapActivity -> {                // 주어진 위도,경도 위치로 지도를 보여줄 수 있도록 인텐트 설정
+                val map_intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:37.565350, 127.01445"))
+                startActivity(map_intent)
+            }
+        }
     }
 }
